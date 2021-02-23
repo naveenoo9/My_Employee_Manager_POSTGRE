be107a213a5bbe5f77a8545346d0b419c827e5ea
naveenoo9 <50537003+naveenoo9@users.noreply.github.com>
2021-02-23 22:53:31 +0530

    Added cross origin

--- src/main/java/com/employeemanager/myapplications/controllers/EmployeeController.java
+++ src/main/java/com/employeemanager/myapplications/controllers/EmployeeController.java
@@ -17,6 +17,7 @@ import com.employeemanager.myapplications.exceptions.ResourceNotFoundException;
 import com.employeemanager.myapplications.model.Employee;
 import com.employeemanager.myapplications.repository.EmployeeRepository;
 
+@CrossOrigin("http://localhost:3000/")
 @RestController
 @RequestMapping(value = "/api/")
 public class EmployeeController {
